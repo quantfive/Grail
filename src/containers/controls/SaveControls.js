1c46ce9facb509f9eee33c9423615d7ad20cc93b
piccoloman <piccoloman@users.noreply.github.com>
2018-07-10 17:23:02 -0700

    removed some unnecessary code

--- src/containers/controls/SaveControls.js
+++ src/containers/controls/SaveControls.js
@@ -18,13 +18,10 @@ import ResultsModal from '../modals/ResultsModal';
 // Config
 import API from '../../config/api';
 import Helpers from '../../config/helpers';
-import EMPTY_CHROME_STYLES from '../../config/empty_chrome_styles';
 
 // Redux
 import { GrailActions } from '../../redux/grail';
 import { ModalActions } from '../../redux/modals';
-
-let oldFetch = window.fetch;
 
 const SKIPTAGS = {
   script: true,
@@ -54,201 +51,22 @@ class SaveControls extends Component {
 
     let resume = sessionStorage.getItem('grail_resume');
     if (resume === 'true') {
-      window.fetch = this.fetch;
+      //window.fetch = this.fetch;
     }
 
     this.addToIgnore = this.addToIgnore.bind(this);
-    this.editIgnore = this.editIgnore.bind(this);
-  }
-
-  getWindow() {
-    return window;
-  }
-
-  getPageName() {
-    return this.getWindow().location.href;
-  }
-
-  getDocument() {
-    return this.getWindow().document;
-  }
-
-  getDocumentHtml() {
-    return this.getDocument().documentElement;
-  }
-
-  getPageState() {
-    return {
-      page_name: this.getPageName(),
-      page_width: this.getWindow().innerWidth,
-      html_elements_attributes: this.getAllStyles(),
-    };
-  }
-
-  // Filters: has id, not script
-  getAllElems (filterById) {
-    let all = this.getDocumentHtml().getElementsByTagName("*");
-    var elems = [];
-    for(let i = 0; i < all.length; i++) {
-      if(!SKIPTAGS[all[i].tagName.toLowerCase()]) {
-        if (filterById) {
-          if (all[i].id !== '') {
-            elems.push(all[i]);
-          }
-        } else {
-          elems.push(all[i]);
-        }
-      }
-    }
-    return elems;
-  }
-
-  getAllStyles() {
-    let all = this.getAllElems(true);
-    var elems = [];
-    for(let i = 0; i < all.length; i++) {
-      let style = {element_id: all[i].id, html: all[i].outerHTML.replace(all[i].innerHTML, ''), css_attributes:{}};
-      // Computed CSS Properties
-      let styles = this.getWindow().getComputedStyle(all[i]);
-      for (let j = 0; j < styles.length; j++) {
-        let value = styles.getPropertyValue(styles[j]);
-        // Don't include default styles to reduce size of styles
-        if(EMPTY_CHROME_STYLES[styles[j]] !== value) {
-          style['css_attributes'][styles[j]] = value;
-        }
-      }
-      // Computed Postion of Element
-      let boundingRect = all[i].getBoundingClientRect();
-      for (let attrname in boundingRect) {
-        if (!isNaN(boundingRect[attrname])) {
-          style['css_attributes']['bounding_' + attrname] = boundingRect[attrname];
-        }
-      }
-      // Text of Element
-      if (all[i].childNodes.length > 0) {
-        style['css_attributes']['text'] = all[i].childNodes[0].nodeValue;
-      }
-      elems.push(style);
-    }
-    return elems;
-  }
-
-  getAllElements() {
-    var body = this.getDocumentHtml().getElementsByTagName("body");
-    let elements = this.getHTMLElements(body, {id: 0});
-    return {'body': elements};
-  }
-
-  getHTMLElements(collection, tempId) {
-    if (collection.length == 0) {
-      return null;
-    }
-    let elements = {};
-    for (let child of collection) {
-      if (child.id !== 'wrapper') {
-        if (!child.id) {
-          child.setAttribute('grail-order', tempId.id);
-        } else {
-          tempId.id = tempId.id - 1;
-        }
-        if (!child.hasChildNodes()) {
-          tempId.id = tempId.id + 1;
-        } else {
-          tempId.id = tempId.id + 1;
-          this.getHTMLElements(child.children, tempId);
-        }
-      }
-    }
-    return elements;
-  }
-
-  getAll() {
-    let all = this.getAllElements();
-    let body = this.getDocumentHtml().getElementsByTagName('body');
-    let html = body[0].outerHTML;
-    let res = {}
-    let css = this.getCSS(body, 0, res);
-    return { html: html, css:css };
-  }
-
-  getCSS(collection, tempId, result) {
-    if (collection.length === 0) {
-      return null;
-    }
-
-    for (let child of collection) {
-      let css = {}
-      let id = child.id ? child.id : child.getAttribute('grail-order')
-      let rect = this.filter(child.getBoundingClientRect());
-
-      // Computed CSS Properties
-      let styles = this.getWindow().getComputedStyle(child);
-      for(let j = 0; j < styles.length; j++) {
-        let value = styles.getPropertyValue(styles[j]);
-        // Don't include default styles to reduce size of styles
-        if(EMPTY_CHROME_STYLES[styles[j]] !== value) {
-          rect[styles[j]] = value;
-        }
-      }
-
-      if (!child.hasChildNodes()) {
-        result[id] = rect;
-        tempId = tempId + 1;
-      } else {
-        result[id] = rect;
-        result = {...result, ...this.getCSS(child.children, tempId + 1, result)};
-        tempId = tempId + 1;
-      }
-    }
-    return result
-  }
-
-  filter(rect) {
-    let dict = {};
-    dict['x'] = rect.x;
-    dict['y'] = rect.y;
-    return dict;
-  }
-
-  compare(json) {
-    let html = json['html'];
-    let css = json['css'];
-    let new_state = this.getAll();
-    console.log(new_state['html'] === html);
-    console.log(new_state);
-  }
-
-  clickSave = (e) => {
-    e.stopPropagation();
-    e.preventDefault();
-    window.scrollTo(0,0);
-
-    let api = API.SAVE_PAGE_STATE;
-    let page_state = this.getAll();
-
-    return fetch(api, API.POST_CONFIG({page_state: page_state}))
-    .then(Helpers.checkStatus)
-    .then(Helpers.parseJSON)
-    .then(json => {
-      console.log(json);
-    });
-  }
-
-  stringify = (value) => {
-    let stringifiedValue = JSON.stringify(value);
-    return stringifiedValue;
   }
 
   addToStorage = (key, value, storage=sessionStorage) => {
     let items = storage.getItem(key);
 
     if (!items) {
-      let valueJson = this.stringify([value]);
+      let valueJson = JSON.stringify([value]);
       storage.setItem(key, valueJson);
     } else {
       let parsedItems = this.retrieveFromStorage(key, storage);
       parsedItems.push(value);
-      let valueJson = this.stringify(parsedItems);
+      let valueJson = JSON.stringify(parsedItems);
       storage.setItem(key, valueJson);
     }
   }
@@ -282,11 +100,11 @@ class SaveControls extends Component {
     if (items && !index) {
       // Key exists in storage
       poppedValue = items.pop();
-      storage.setItem(key, this.stringify(items));
+      storage.setItem(key, JSON.stringify(items));
     } else if (items && index) {
       // Key exists in storage and index given
       poppedValue = items.pop(index);
-      storage.setItem(key, this.stringify(items));
+      storage.setItem(key, JSON.stringify(items));
     }
     return poppedValue;
   }
@@ -317,7 +135,7 @@ class SaveControls extends Component {
    * Gets all clickable elements on the page
    */
   getAllClickableElements = () => {
-    let allElements = this.getDocument().querySelectorAll(':not([class^=grailTest])');
+    let allElements = document.querySelectorAll(':not([class^=grailTest])');
     let filteredElements = [];
 
     for (let i = 0; i < allElements.length; i++) {
@@ -336,10 +154,11 @@ class SaveControls extends Component {
   startClickAll = () => {
     let elements = this.getAllClickableElements();
 
+    debugger;
     xhook.enable()
     xhook.before(this.xmlBeforeHook);
     xhook.after(this.xmlAfterHook);
-    window.fetch = this.fetch;
+    //window.fetch = this.fetch;
 
     this.setState({
       elements,
@@ -360,13 +179,6 @@ class SaveControls extends Component {
   resume = () => {
     this.setState({paused: false});
     this.clickAllElements();
-  }
-
-  /***
-   * Replace all commas
-   */
-  replaceCommas = (outerHTML) => {
-    outerHTML.replace(new RegExp(','))
   }
 
   /***
@@ -443,7 +255,7 @@ class SaveControls extends Component {
     // let elements = this.state.elements;
     let element = elements.pop();
     let start = this.state.start;
-    window.fetch = this.fetch;
+    //window.fetch = this.fetch;
 
     if (element !== null && element !== undefined && !this.state.paused && start) {
       let currentHref = window.location.href;
@@ -510,7 +322,6 @@ class SaveControls extends Component {
   }
 
   resetGrail = () => {
-    window.fetch = oldFetch;
     xhook.disable();
     this.setState({start: false})
   }
@@ -564,23 +375,70 @@ class SaveControls extends Component {
     );
   }
 
-  takeSnapshot = () => {
-    let { grailActions, grail } = this.props;
-    window.scrollTo(0,0);
-
-    let page_state = this.getAll();
-    if (grail.recording) {
-      grailActions.recordEvent({
-        snapshot: page_state
-      });
+  /***
+  * Starts fetch timer when a request is made
+  * @params request - Unused request object
+  */
+  xmlBeforeHook = (request) => {
+    if (this.state.start) {
+      let { grailActions } = this.props;
+      let api = request.url;
+      grailActions.beforeFetch(api);
     }
-
-    return page_state;
   }
 
-  editIgnore = (event) => {
-    // Old functionality
-    this.setState({ignoreElements: event.target.value});
+  /***
+  * Checks for errors and if all fetches are finished
+  * @params request - Request object
+  * @params response - Response object
+  */
+  xmlAfterHook = (request, response) => {
+    let { grailActions } = this.props;
+    if (this.state.start) {
+      let api = request.url;
+      let body = request.body;
+      let method = request.method;
+      let data = {
+        method: request.method,
+        body: request.body,
+        headers: request.headers,
+      }
+
+      let event = {
+        endpoint: api,
+        request_input: body ? body : null,
+        request_type: method,
+        request_output: response,
+      }
+
+      if (response.status >= 400 || response.status === 0 || response instanceof Error) {
+        let error = response.statusText;
+
+        if(!error) {
+          error = Response.toString();
+        }
+        this.saveError(api, data, error);
+      }
+
+      setTimeout(() => {
+        grailActions.fetchFinished(api);
+      }, 200)
+    }
+  }
+
+  /***
+   * Records the frontend error and saves it to sessionStorage
+   * @params error e -- the error object
+   */
+  recordFrontendError = (e) => {
+    this.addToStorageByPage('grail-frontend-errors', {
+      stack: e.error.stack,
+      message: e.message,
+      filename: e.filename,
+      lineno: e.lineno
+    },
+    window.location.pathname
+    );
   }
 
   /***
@@ -681,262 +539,6 @@ class SaveControls extends Component {
     }
   }
 
-  getPlayBack = async () => {
-    let { grail, grailActions } = this.props;
-    window.fetch = this.fetch
-    window.scrollTo(0,0);
-
-    let api = API;
-    this.takeSnapshot();
-    let states = await grailActions.playback();
-
-    let first = states.playback[0];
-    this.playback(first);
-  }
-
-  /***
-   * Plays back a specific state of the app
-   */
-  playback = (pageState) => {
-    let { grailActions } = this.props;
-    let id = pageState.action_params.id;
-    let element;
-    let cur_html = this.getAllElements();
-    if (id) {
-      element = document.getElementById(id);
-    } else {
-      let order = pageState.action_params.order;
-      element = document.querySelectorAll(`[grail-order="${order}"]`)[0];
-    }
-
-    switch(pageState.action_name) {
-      case 'click':
-        element.click()
-        this.snapshotTimeout = setTimeout(() => {
-          let page_state = this.takeSnapshot();
-          grailActions.checkHTML({
-            cur_html: page_state.html,
-            cur_css: page_state.css,
-            page_state_id: pageState.id,
-          });
-        }, 500);
-        break;
-      default:
-        console.log('no action');
-    }
-  }
-
-  clickCheck = (e) => {
-    let { grailActions, modalActions } = this.props;
-    e.stopPropagation();
-    e.preventDefault();
-    window.scrollTo(0,0);
-
-    let page_state = this.getAll();
-    let api = API.DIFF_PAGE_STATE;
-    let isGrail = true;
-
-    grailActions.checkPage(api, page_state, isGrail);
-    modalActions.openCheckModal(true);
-  }
-
-  checkReady = (e) => {
-    e.stopPropagation();
-    e.preventDefault();
-    window.scrollTo(0,0);
-
-    let res = fetch(API.CHECK_READY, API.POST_CONFIG({test: null}))
-    .then(Helpers.checkStatus)
-    .then(Helpers.parseJSON)
-    .then(json => {
-      console.log(json);
-      console.log(this.getDocument().readyState)
-    });
-  }
-
-  recordToggle = () => {
-    let { grail, grailActions } = this.props;
-    window.fetch = this.fetch
-    grailActions.toggleRecord();
-    if (this.state.isRecording && grail.recordedSession.length > 0) {
-      grailActions.saveEvent()
-    } else if(!this.state.isRecording) {
-      this.takeSnapshot()
-    }
-    this.setState({
-      isRecording: !this.state.isRecording,
-    })
-  }
-
-  fetch = async (api, data, isGrail=false) => {
-    let { grailActions } = this.props;
-    clearTimeout(this.snapshotTimeout);
-    this.setState({
-      fetchMade: true,
-    })
-    if (isGrail) {
-      return oldFetch(api, data)
-    } else {
-      grailActions.beforeFetch(api);
-      let response = null;
-      let resClone = null;
-      let res = null;
-      try {
-        response = await oldFetch(api, data)
-        resClone = response.clone()
-        res = await Helpers.parseJSON(resClone)
-
-      } catch (error) {
-        console.log(error);
-      }
-      let event = {
-        endpoint: api,
-        request_input: data.body ? data.body : null,
-        request_type: data.method,
-        request_output: res ? res : null,
-      }
-      await grailActions.recordEvent(event);
-      this.setState({
-        fetchMade: false,
-      })
-
-      // The timeout for fetch being done is here so the element can render properly
-      setTimeout(() => {
-        grailActions.fetchFinished(api);
-      }, 200)
-
-      return response;
-    }
-  }
-
-  /***
-  * Starts fetch timer when a request is made
-  * @params request - Unused request object
-  */
-  xmlBeforeHook = (request) => {
-    if (this.state.start) {
-      let { grailActions } = this.props;
-      let api = request.url;
-      grailActions.beforeFetch(api);
-    }
-  }
-
-  /***
-  * Checks for errors and if all fetches are finished
-  * @params request - Request object
-  * @params response - Response object
-  */
-  xmlAfterHook = (request, response) => {
-    let { grailActions } = this.props;
-    if (this.state.start) {
-      let api = request.url;
-      let body = request.body;
-      let method = request.method;
-      let data = {
-        method: request.method,
-        body: request.body,
-        headers: request.headers,
-      }
-
-      let event = {
-        endpoint: api,
-        request_input: body ? body : null,
-        request_type: method,
-        request_output: response,
-      }
-
-      if (response.status >= 400 || response.status === 0 || response instanceof Error) {
-        let error = response.statusText;
-
-        if(error === null || error === undefined) {
-          error = response.toString();
-        }
-        this.saveError(api, data, error);
-      }
-
-      setTimeout(() => {
-        grailActions.fetchFinished(api);
-      }, 200)
-    }
-  }
-
-
-  recordMouseEvents = async (e) => {
-    let { grailActions } = this.props;
-    if (this.state.isRecording && !this.state.firstClick) {
-      if (e.type === 'click') {
-        let element = e.path[0]
-        if (element instanceof SVGElement) {
-          element = e.path[1]
-        }
-
-        let clickElement = element;
-        for (let i = 0; i < e.path.length; i++) {
-          if (e.path[i].onclick) {
-            clickElement = e.path[i];
-          }
-        }
-
-        let event = {
-          page_name: window.location.href,
-          action_name: 'click',
-          action_params: {
-            id: element.id !== "" ? element.id : null,
-            order: element.attributes['grail-order'] ? element.attributes['grail-order'].value : null,
-            outerHTML: element.outerHTML,
-            clickHTML: clickElement.outerHTML,
-          },
-        }
-
-        await grailActions.recordEvent(event)
-        if (!this.state.fetchMade) {
-          this.snapshotTimeout = setTimeout(async () => {
-            this.takeSnapshot();
-            grailActions.addEventToList();
-          }, 500);
-        }
-
-        this.setState({
-          fetchMade: false,
-        })
-
-        //console.log(`Click event occured at (x: ${e.clientX} y: ${e.clientY})`)
-      } else if (e.type === 'mousemove') {
-        //console.log(`Current Mouse Position: (x: ${e.clientX} y: ${e.clientY})`)
-      }
-    } else if (this.state.isRecording && this.state.firstClick) {
-      this.setState({
-        firstClick: false,
-      })
-    } else {
-      this.setState({
-        firstClick: true,
-      })
-    }
-  }
-
-  fetchListener = (event) => {
-    event.respondWith(
-      new Response("Response body", { headers: { "Content-Type" : "text/plain" }})
-    )
-  }
-
-  /***
-   * Records the frontend error and saves it to sessionStorage
-   * @params error e -- the error object
-   */
-  recordFrontendError = (e) => {
-    this.addToStorageByPage('grail-frontend-errors', {
-      stack: e.error.stack,
-      message: e.message,
-      filename: e.filename,
-      lineno: e.lineno,
-      element: this.state.currentElement.outerHTML,
-    },
-    window.location.pathname
-    );
-  }
-
   componentDidMount() {
     window.addEventListener('error', this.recordFrontendError, false);
     let { grail } = this.props;
@@ -954,7 +556,6 @@ class SaveControls extends Component {
     let { grail, grailActions, modalActions } = this.props;
     if (grail.activeFetchCalls.length === 0 && prevProps.grail.activeFetchCalls.length > 0) {
       if (grail.recording) {
-        let page_state = this.takeSnapshot();
         grailActions.addEventToList();
       } else {
         if (grail.playback.length > 0) {
